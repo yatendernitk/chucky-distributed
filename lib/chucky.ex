76a07cceb601297fae4c59bf26fd5fd124131874
ok <ok@yimac.local>
2018-01-30 17:24:11 +0530

    rajni server completed

--- lib/chucky.ex
+++ lib/chucky.ex
@@ -1,4 +1,7 @@
 defmodule Chucky do
+  use Application
+  require Logger
+
   @moduledoc """
   Documentation for Chucky.
   """
@@ -14,5 +17,30 @@ defmodule Chucky do
   """
   def hello do
     :world
+  end
+
+  def start(type, _args) do
+    import Supervisor.Spec
+    IO.inspect type
+    children = [
+      worker(Chucky.Server, [])
+    ]
+    case type do
+      :normal ->
+        Logger.info("Application is started on #{node()}")
+
+      {:takeover, old_node} ->
+        Logger.info("#{node()} is taking over #{old_node}")
+
+      {:failover, old_node} ->
+        Logger.info("#{old_node} is failing over to #{node()}")
+    end
+
+    opts = [strategy: :one_for_one, name: {:global, Chucky.Supervisor}]
+    Supervisor.start_link(children, opts)
+  end
+
+  def fact do
+    Chucky.Server.fact()
   end
 end
